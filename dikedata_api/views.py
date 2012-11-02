fac31056df1374cc548001eea8aded188a7c5eb9
Berto Booijink <berto.booijink@nelen-schuurmans.nl>
2012-11-02 14:27:42 +0100

    Directly write to Cassandra from the api. Also improved error flow.

--- dikedata_api/views.py
+++ dikedata_api/views.py
@@ -11,71 +11,99 @@ from lizard_ui.views import UiView
 from cassandralib.models import CassandraDataStore
 from rabbitmqlib.models import Producer
 
+import pandas as pd
 import pytz
 import uuid
 
 
+SERVERS = settings.CASSANDRA['servers']
+KEYSPACE = settings.CASSANDRA['keyspace']
+COL_FAM = settings.CASSANDRA['column_family']
+COLNAME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+INTERNAL_TIMEZONE = pytz.UTC
+
 def api_response(request):
-    SERVERS = settings.CASSANDRA['servers']
-    KEYSPACE = settings.CASSANDRA['keyspace']
-    COL_FAM = settings.CASSANDRA['column_family']
-    COLNAME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
-    INTERNAL_TIMEZONE = pytz.UTC
-    
     cassandra = CassandraDataStore(SERVERS, KEYSPACE, COL_FAM, 10000)
     
-    out = {}
+    msg = {}
     get = request.GET.keys()
     try:
-        if 'request' in get:
-            params = json.loads(request.GET.get('request'))
-            if 'start' in params:
-                start = datetime.strptime(params['start'], COLNAME_FORMAT)
-            else:
-                start = datetime.now() + relativedelta( years = -3 )
-            if 'end' in params:
-                end = datetime.strptime(params['end'], COLNAME_FORMAT)
-            else:
-                end = datetime.now()
-            if "observers" in params:
-                out['observers'] = {}
-                for observer_id in params['observers']:
-                    filter = ['value', 'flag']
+        if not 'request' in get:
+            raise Exception("Missing GET parameter 'request'")
+        params = json.loads(request.GET.get('request'))
+        if not 'observers' in params:
+            raise Exception("Missing parameter 'observers'")
+        msg['observers'] = {}
+        if 'start' in params:
+            start = datetime.strptime(params['start'], COLNAME_FORMAT)
+        else:
+            start = datetime.now() + relativedelta( years = -3 )
+        if 'end' in params:
+            end = datetime.strptime(params['end'], COLNAME_FORMAT)
+        else:
+            end = datetime.now()
+        for observer_id in params['observers']:
+            filter = ['value', 'flag']
+    
+            df = cassandra.read(observer_id,
+                INTERNAL_TIMEZONE.localize(start),
+                INTERNAL_TIMEZONE.localize(end), params=filter)
             
-                    df = cassandra.read(observer_id,
-                        INTERNAL_TIMEZONE.localize(start),
-                        INTERNAL_TIMEZONE.localize(end), params=filter)
-                    
-                    data = [
-                        dict([('datetime', timestamp.strftime(COLNAME_FORMAT))] + [
-                            (colname, row[i])
-                            for i, colname in enumerate(df.columns)
-                        ])
-                        for timestamp, row in df.iterrows()
-                    ]
-                    out['observers'][observer_id] = data
+            data = [
+                dict([('datetime', timestamp.strftime(COLNAME_FORMAT))] + [
+                    (colname, row[i])
+                    for i, colname in enumerate(df.columns)
+                ])
+                for timestamp, row in df.iterrows()
+            ]
+            msg['observers'][observer_id] = data
 
     except Exception as ex:
-        out['errors'] = ex.arg
+        msg['status'] = ex.__class__.__name__
+        msg['errors'] = ex.args
 
-    return HttpResponse(json.dumps(out, indent=4), mimetype='application/json')
+    return HttpResponse(json.dumps(msg, indent=4), mimetype='application/json')
 
 def api_write(request):
-    out = {}
+    msg = {}
     get = request.GET.keys()
     try:
-        if 'request' in get:
-            params = json.loads(request.GET.get('request'))
-            if 'observers' in params:
-                observers = params['observers']
-                producer = Producer(settings.RABBITMQ['server'],
-                    settings.RABBITMQ['user'], settings.RABBITMQ['password'],
-                    settings.RABBITMQ['vhost'])
-                msg_id = uuid.uuid4()
-                out = {'message_id': str(msg_id), 'observers': observers}
-                producer.send(out, b"timeseries", b"store")
-                out['status'] = 'sent'
-    except Exception as ex:
-        out['errors'] = ex.args
+        if not 'request' in get:
+            raise Exception("Missing GET parameter 'request'")
+        params = json.loads(request.GET.get('request'))
+        if not 'observers' in params:
+            raise Exception("Missing parameter 'observers'")
+        observers = params['observers']
+        msg = {'observers': observers}
 
-    return HttpResponse(json.dumps(out, indent=4), mimetype='application/json')
+        # Store time series in Cassandra
+        for obs_id in observers:
+            datetimes = []
+            data = []
+            for datum in observers[obs_id]:
+                if "datetime" in datum:
+                    dt = datetime.strptime(datum["datetime"], COLNAME_FORMAT)
+                    datetimes.append(dt.replace(tzinfo=INTERNAL_TIMEZONE))
+                    del datum["datetime"]
+                    data.append(datum)
+            df = pd.DataFrame(data=data, index=datetimes)
+            cassandra = CassandraDataStore(
+                settings.CASSANDRA['servers'],
+                settings.CASSANDRA['keyspace'],
+                settings.CASSANDRA['column_family'],
+                10000
+            )
+            cassandra.write(obs_id, df)
+
+        # Inform Rabbit MQ
+        msg['message_id'] = str(uuid.uuid4())
+        producer = Producer(settings.RABBITMQ['server'],
+            settings.RABBITMQ['user'], settings.RABBITMQ['password'],
+            settings.RABBITMQ['vhost'])
+        producer.send(msg, b"timeseries", b"store")
+        msg['status'] = 'OK'
+    except Exception as ex:
+        msg['status'] = ex.__class__.__name__
+        msg['errors'] = ex.args
+
+    return HttpResponse(json.dumps(msg, indent=4), mimetype='application/json')
