70a1b2ddd4501ce898efd9432a023ce232a5c98f
Berto Booijink <berto.booijink@nelen-schuurmans.nl>
2012-11-21 16:27:52 +0100

    Versioned API as v0.

--- dikedata_api/urls.py
+++ dikedata_api/urls.py
@@ -4,49 +4,16 @@ from django.conf.urls.defaults import include
 from django.conf.urls.defaults import patterns
 from django.conf.urls.defaults import url
 from django.contrib import admin
+from django.views.generic.simple import redirect_to
 from lizard_ui.urls import debugmode_urlpatterns
-from rest_framework.urlpatterns import format_suffix_patterns
 
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^$',
-        views.Root.as_view()),
-    url(r'^users/$',
-        views.UserList.as_view(),
-        name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$',
-        views.UserDetail.as_view(),
-        name='user-detail'),
-    url(r'^groups/$',
-        views.GroupList.as_view(),
-        name='usergroup-list'),
-    url(r'^groups/(?P<pk>[0-9]+)/$',
-        views.GroupDetail.as_view(),
-        name='usergroup-detail'),
-    url(r'^locations/$',
-        views.LocationList.as_view(),
-        name='location-list'),
-    url(r'^locations/(?P<pk>[^/]+)/$',
-        views.LocationDetail.as_view(),
-        name='location-detail'),
-    url(r'^timeseries/$',
-        views.TimeseriesList.as_view(),
-        name='timeseries-list'),
-    url(r'^timeseries/(?P<pk>[^/]+)/$',
-        views.TimeseriesDetail.as_view(),
-        name='timeseries-detail'),
-    url(r'^timeseries/(?P<pk>[^/]+)/data/$',
-        views.TimeseriesData.as_view(),
-        name='timeseries-data'),
-#    url(r'^read/', views.api_response),
-#    url(r'^write/', views.api_write),
-)
-
-urlpatterns += patterns(
-    '',
+    url(r'^$', redirect_to, {'url': 'v0'}),
+    url(r'^v0/', include('dikedata_api.urls_v0'), name='v0'),
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
 
---
+++ dikedata_api/urls_v0.py
@@ -0,0 +1,40 @@
+# (c) Nelen & Schuurmans.  MIT licensed, see LICENSE.rst.
+from dikedata_api import views
+from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import url
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^$',
+        views.Root.as_view()),
+    url(r'^users/$',
+        views.UserList.as_view(),
+        name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$',
+        views.UserDetail.as_view(),
+        name='user-detail'),
+    url(r'^groups/$',
+        views.GroupList.as_view(),
+        name='usergroup-list'),
+    url(r'^groups/(?P<pk>[0-9]+)/$',
+        views.GroupDetail.as_view(),
+        name='usergroup-detail'),
+    url(r'^locations/$',
+        views.LocationList.as_view(),
+        name='location-list'),
+    url(r'^locations/(?P<pk>[^/]+)/$',
+        views.LocationDetail.as_view(),
+        name='location-detail'),
+    url(r'^timeseries/$',
+        views.TimeseriesList.as_view(),
+        name='timeseries-list'),
+    url(r'^timeseries/(?P<pk>[^/]+)/$',
+        views.TimeseriesDetail.as_view(),
+        name='timeseries-detail'),
+    url(r'^timeseries/(?P<pk>[^/]+)/data/$',
+        views.TimeseriesData.as_view(),
+        name='timeseries-data'),
+#    url(r'^read/', views.api_response),
+#    url(r'^write/', views.api_write),
+)
